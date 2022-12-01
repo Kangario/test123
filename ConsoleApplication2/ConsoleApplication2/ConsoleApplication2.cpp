936bd3185671e822bf50b0a280b705bb6d8f768d
Kangario <68245183+Kangario@users.noreply.github.com>
2022-12-01 18:15:33 +0300

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,29 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+   
+    int n,a;
+    
+
+
+
+
+
+    /*
+    int check = 0;
+    cout << "Вы проснулись у дороги, в глазах темнело. \n";
+    cout << "Пойти по дороге? \n  1-да 0-нет\n";
+    cin >> check;
+    if (check == 1) {
+     
+    }
+    else {
+        cout << "Лежит";
+    }
+     */
+}
+
